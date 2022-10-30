9f9fe9f1bdbd8a9b4d8c526faa7eb46b97ef0d04
Justin-Codes <111461352+JustinEdobor@users.noreply.github.com>
2022-10-30 04:34:09 +0100

    Add files via upload

---
+++ build/precache-manifest.88be241bee0c928454b62a645644e806.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "eee5d5f95525ab48c8f0",
+    "url": "/static/css/main.f4353e2b.chunk.css"
+  },
+  {
+    "revision": "eee5d5f95525ab48c8f0",
+    "url": "/static/js/main.eee5d5f9.chunk.js"
+  },
+  {
+    "revision": "60861e190ac2c61489a8",
+    "url": "/static/js/1.60861e19.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96367979ad0165c0719c7a3fbe05b98e",
+    "url": "/static/media/song1.96367979.png"
+  },
+  {
+    "revision": "46ae2f07b46cb0c537ca4e250f81058c",
+    "url": "/static/media/song2.46ae2f07.png"
+  },
+  {
+    "revision": "c8774466546ee314d8f962505df00fa4",
+    "url": "/static/media/song3.c8774466.png"
+  },
+  {
+    "revision": "c908651277a82f7942d9d68929dad089",
+    "url": "/static/media/photo.c9086512.png"
+  },
+  {
+    "revision": "053eb5e56fd3cd9b2d1cff0a8447d1ac",
+    "url": "/static/media/Frame 4.053eb5e5.png"
+  },
+  {
+    "revision": "f2b6a051a97d0b3dc4b96c06b041de79",
+    "url": "/static/media/song4.f2b6a051.png"
+  },
+  {
+    "revision": "da83a35adc60ab7c70326c4f5d266513",
+    "url": "/static/media/song5.da83a35a.png"
+  },
+  {
+    "revision": "79c7f04a776b8f99fd5970cc39bd09d2",
+    "url": "/static/media/song6.79c7f04a.png"
+  },
+  {
+    "revision": "84bd4db121be85f4bc703bcc6a66e3be",
+    "url": "/static/media/Vector.84bd4db1.svg"
+  },
+  {
+    "revision": "28968553f45ea40109446ab16aa5d557",
+    "url": "/index.html"
+  }
+];
