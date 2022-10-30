a55a8d0c01d8ace89d65eee5353d9f158805054c
Justin-Codes <111461352+JustinEdobor@users.noreply.github.com>
2022-10-30 05:30:47 +0100

    Delete spotify.js

--- src/components/spotify.js
+++
@@ -1,5 +0,0 @@
-const CLIENT_ID = "+++++++++++++++++++++++++++++"
-const REDIRECT_URI = "http://localhost:3000"
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-const RESPONSE_TYPE = "token"
-<a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
